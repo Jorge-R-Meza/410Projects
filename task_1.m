edb0989c480a4fcac9a242c22278c2f5f61ae3de
Jorge Meza <121531858+Jorge-R-Meza@users.noreply.github.com>
2023-10-29 21:07:03 -0400

    added export to task_1

--- task_1.m
+++ task_1.m
@@ -1,19 +1,30 @@
 % Load camera parameters and image
-load Parameters_V2_1.mat
+load Parameters_V1_1.mat
 cam1 = Parameters;
-image1 = imread("im2corrected.jpg");
+image1 = imread("im1corrected.jpg");
+
+load Parameters_V2_1.mat
+cam2 = Parameters;
+image2 = imread("im2corrected.jpg");
 % Load mocap points (world coordinates) and transform them to camera coordinates
 load mocapPoints3D.mat
 pts3D = reshape(pts3D, [3, 39]);
-R = cam1.Rmat;
-C = cam1.position';
-PC = R * (pts3D - C);
+R1 = cam1.Rmat;
+C1 = cam1.position';
+PC1 = R1 * (pts3D - C1);
+
+R2 = cam2.Rmat;
+C2 = cam2.position';
+PC2 = R2 * (pts3D - C2);
+
 % Initialization
-pixelCoords = zeros(2, 39);
+pixelCoords1 = zeros(2, 39);
+pixelCoords2 = zeros(2, 39);
 prinpoint1 = cam1.prinpoint;
+prinpoint2 = cam2.prinpoint;
 % Project camera coordinates to 2D pixel locations
 for x = 1:39
-    points = PC(:, x);
+    points = PC1(:, x);
     
     mat1 = [
         cam1.foclen 0 0 0;
@@ -24,43 +35,72 @@ for x = 1:39
     
     prod = mat1 * mat2;
     
-    pixelCoords(1, x) = prod(1)/prod(3) + prinpoint1(1);
-    pixelCoords(2, x) = prod(2)/prod(3) + prinpoint1(2);
+    pixelCoords1(1, x) = prod(1)/prod(3) + prinpoint1(1);
+    pixelCoords1(2, x) = prod(2)/prod(3) + prinpoint1(2);
 end
-% Display the image and overlay the 2D points
+
+for x = 1:39
+    points = PC2(:, x);
+    
+    mat1 = [
+        cam2.foclen 0 0 0;
+        0 cam2.foclen 0 0;
+        0 0 1 0];
+    
+    mat2 = [points; 1];
+    
+    prod = mat1 * mat2;
+    
+    pixelCoords2(1, x) = prod(1)/prod(3) + prinpoint2(1);
+    pixelCoords2(2, x) = prod(2)/prod(3) + prinpoint2(2);
+end
+fig1 = figure;
+ax1 = axes('Parent', fig1);
+imshow(image1, 'Parent', ax1);
+hold(ax1, 'on');
+plot(ax1, pixelCoords1(1, :), pixelCoords1(2, :), 'go');
+% Label the points
+for i = 1:size(pixelCoords1, 2)
+    text(pixelCoords1(1, i), pixelCoords1(2, i), num2str(i), 'Color', 'red', 'Parent', ax1);
+end
+hold(ax1, 'off');
+
+fig2 = figure;
+ax2 = axes('Parent', fig2);
+imshow(image2, 'Parent', ax2);
+hold(ax2, 'on');
+plot(ax2, pixelCoords2(1, :), pixelCoords2(2, :), 'go');
+% Label the points
+for i = 1:size(pixelCoords2, 2)
+    text(pixelCoords2(1, i), pixelCoords2(2, i), num2str(i), 'Color', 'red', 'Parent', ax2);
+end
+hold(ax2, 'off');
+save('pixelCoordinates.mat', 'pixelCoords1', 'pixelCoords2');
+
+%... [rest of your code]
+%{
+% Display the first image and overlay the 2D points
+figure; % <- This line opens a new figure
 imshow(image1);
 hold on;
-plot(pixelCoords(1, :), pixelCoords(2, :), 'go');
+plot(pixelCoords1(1, :), pixelCoords1(2, :), 'go');
 % Label the points
-for i = 1:size(pixelCoords, 2)
-    text(pixelCoords(1, i), pixelCoords(2, i), num2str(i), 'Color', 'red');
+for i = 1:size(pixelCoords1, 2)
+    text(pixelCoords1(1, i), pixelCoords1(2, i), num2str(i), 'Color', 'red');
 end
 hold off;
-%{
-imshow('im1corrected.jpg');
-axis on
+
+% Display the second image and overlay the 2D points
+figure; % <- This line opens another new figure
+imshow(image2);
 hold on;
-% Plot cross at row 100, column 50
-plot(50,100, 'r+', 'MarkerSize', 30, 'LineWidth', 2);
-maxX = max(pixelCoords(1, :));
-maxY = max(pixelCoords(2, :));
-[imgHeight, imgWidth, ~] = size(image1);
-disp(['Max X: ', num2str(maxX), ' Image Width: ', num2str(imgWidth)]);
-disp(['Max Y: ', num2str(maxY), ' Image Height: ', num2str(imgHeight)]);
+plot(pixelCoords2(1, :), pixelCoords2(2, :), 'go');
+% Label the points
+for i = 1:size(pixelCoords2, 2)
+    text(pixelCoords2(1, i), pixelCoords2(2, i), num2str(i), 'Color', 'red');
+end
+hold off;
 %}
 
-%{
-imshow('im1corrected.jpg');
-axis on
-hold on;
-% Plot cross at row 100, column 50
-plot(50,100, 'r+', 'MarkerSize', 30, 'LineWidth', 2);
-
-maxX = max(pixelCoords(1, :));
-maxY = max(pixelCoords(2, :));
-[imgHeight, imgWidth, ~] = size(image1);
-disp(['Max X: ', num2str(maxX), ' Image Width: ', num2str(imgWidth)]);
-disp(['Max Y: ', num2str(maxY), ' Image Height: ', num2str(imgHeight)]);
-%}
 
 
--- task_2.m
+++ task_2.m
@@ -1,3 +1,5 @@
 image1 = load("im1corrected.jpg")
 image2 = load("im2corrected.jpg")
 
+run ("task_1.m")
+
